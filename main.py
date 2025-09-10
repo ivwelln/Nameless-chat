4f602bce60ad3bd7bc475cbe04ee5e0fe41fddf2
ivwelln <151129749+ivwelln@users.noreply.github.com>
2025-09-10 13:34:25 +0500

    Added index and auth htmls

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 import uvicorn
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name = "static")
 
 templates = Jinja2Templates(directory="templates")
 
