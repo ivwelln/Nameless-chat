dc2a9d9ea2e598658881efde65222fb617918597
ivwelln <151129749+ivwelln@users.noreply.github.com>
2025-09-14 17:10:03 +0500

    Auth page

--- main.py
+++ main.py
@@ -16,48 +16,32 @@ async def root(request: Request):
 @app.get("/auth", response_class=HTMLResponse)
 async def auth_get(request: Request):
     return templates.TemplateResponse("auth.html", {
-        "request": request,
-        "mode": None,
-        "message": None,
-        "alert": None
+        "request": request
     })
     
-@app.post("/auth", response_class=HTMLResponse)
-async def auth_post(
-    request: Request,
+@app.post("/auth/check-email")
+async def check_email(email: str = Form(...)):
+    if (email.lower() == "test@gmail.com"):
+        return {"mode" : "login", "message" : "Enter your account."}
+    else:
+        return {"mode" : "register", "message" : "Create new account."}
+
+@app.post("/auth/submit")
+async def sumbit(
     email: str = Form(...),
-    password: str = Form(None),
+    password: str = Form(...),
     repeatedPassword: str = Form(None)
 ):
-    if email == "test@gmail.com":
-        message = "Enter your account."
-        if password:
-            return templates.TemplateResponse("index.html", {"request": request})  # Login success!
-        return templates.TemplateResponse("auth.html", {
-            "request": request,
-            "mode": "login",
-            "message": message,
-            "alert": None
-        })
-    else:
-        message = "Create new account."
-        alert = None
-        if password and repeatedPassword:
-            if password != repeatedPassword:
-                alert = "Passwords don't match."
-                return templates.TemplateResponse("auth.html", {
-                    "request": request,
-                    "mode": "register",
-                    "alert": alert,
-                    "message": message
-                })
-            return templates.TemplateResponse("index.html", {"request": request})
-        return templates.TemplateResponse("auth.html", {
-            "request": request,
-            "mode": "register",
-            "alert": alert,
-            "message": message
-        })
+    if (email == "test@gmail.com"):
+        if (password == "123"):
+            return {"success" : True, "redirect" : "/"}
+        else:
+            return {"success" : False, "alert" : "Wrong password."}
+    else: 
+        if (password != repeatedPassword):
+            return {"success": False, "alert" : "Passwords don't match."}
+        else: 
+            return {"success": True, "redirect" : "/"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
